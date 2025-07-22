74edfffba56c0eea0f2f7393124914a2fd475ee3
Leandro Grison <lgrison@gmail.com>
2025-07-22 15:39:39 -0300

    Criação do projeto

---
+++ src/custom-webpack.config.ts
@@ -0,0 +1,11 @@
+import { EnvironmentPlugin } from 'webpack';
+
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  plugins: [
+    new Dotenv({
+      systemvars: true
+    })
+  ],
+};
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://qtkytnwegxnhyylckzao.supabase.co',
+  apiKey: process.env['SUPABASE_KEY'] ?? '',
+  storageUrl: 'https://qtkytnwegxnhyylckzao.supabase.co/storage/v1/object/public/'
+};
