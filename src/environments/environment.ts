d73f808cb6acda91cf65ed8b5e7a2e37566c26ca
Leandro Grison <lgrison@gmail.com>
2025-07-23 13:16:02 -0300

    Teste de SSR

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://qtkytnwegxnhyylckzao.supabase.co',
-  apiKey: process.env['SUPABASE_KEY'] ?? '',
+  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0a3l0bndlZ3huaHl5bGNremFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA3NTQzNjQsImV4cCI6MjA1NjMzMDM2NH0.0OMR0iWl8FAUXK4K15yJuNRzRqvQcTu-s2d0XBVjuf0',
   storageUrl: 'https://qtkytnwegxnhyylckzao.supabase.co/storage/v1/object/public/'
 };
