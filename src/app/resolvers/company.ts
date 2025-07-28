1674ccb011bad12598952ba07537ecec36dcee6b
Leandro Grison <lgrison@gmail.com>
2025-07-28 10:25:05 -0300

    Teste de captura de url

--- src/app/resolvers/company.ts
+++ src/app/resolvers/company.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT, Inject, Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Company } from '../services/company';
 import { Observable } from 'rxjs';
@@ -6,18 +6,22 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class CompanyResolver implements Resolve<any> {
 
-  constructor(private readonly company: Company) {}
+  constructor(
+    private readonly company: Company,
+    @Inject(DOCUMENT) private readonly document: Document
+  ) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<any>|Promise<any> {
     console.log('CompanyResolver activated for route:', route);
-    console.log('Current state:', state.url);
+    const window = this.document.defaultView as Window;
+    console.log('Window:', window.location.hostname);
     let domain = '';
-    // if (location.hostname === 'localhost') {
-    //   domain = 'demo';
-    // }
+    if (window.location.hostname === 'localhost') {
+      domain = 'demo';
+    }
     return this.company.getCompanyByDomain(domain);
   }
 }
